c26f527c36a1173730adf9a44f399089fb076469
SamuelValentin <samuellv89@gmail.com>
2022-08-19 15:02:38 -0300

    multicast ap1

---
+++ Aplicacao_1/multicast.py
@@ -0,0 +1,56 @@
+# chat server using multicast
+# python fork of the original ruby implementation
+# http://tx.pignata.com/2012/11/multicast-in-ruby-building-a-peer-to-peer-chat-system.html
+# send.py
+# usage : $ python send.py message
+
+from email import message
+import socket
+import struct
+import sys
+
+import threading
+import time
+
+def recive(on):
+    print(on + " \n")
+    
+    multicast_addr = '228.0.0.1'
+    bind_addr = '0.0.0.0'
+    port = 3000
+    i = 0
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    membership = socket.inet_aton(multicast_addr) + socket.inet_aton(bind_addr)
+
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, membership)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    sock.bind((bind_addr, port))
+
+    for i in range(3):
+        message, address = sock.recvfrom(255)
+        print (message)
+
+
+# --------------------------
+def send(message):
+    # message = sys.argv[1] if len(sys.argv) > 1 else 'message via multicast'
+    # message = "Mengo"
+
+    multicast_addr = '228.0.0.1'
+    port = 3000
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ttl = struct.pack('b', 1)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+    sock.sendto(message.encode(), (multicast_addr, port))
+    sock.close()
+    
+    
+
+t = threading.Thread(target=recive,args=("thread sendo executada",))
+t.start()
+
+send("mengo")
+
