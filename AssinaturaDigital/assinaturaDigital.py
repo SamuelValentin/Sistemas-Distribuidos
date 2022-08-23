8ad5c9a0b2e0afbb9edc888a1e1d84f0b78b8b83
SamuelValentin <samuellv89@gmail.com>
2022-08-23 17:39:53 -0300

    Dsenvolvendo Assinatura digital. Problema na funcao keypair

---
+++ AssinaturaDigital/assinaturaDigital.py
@@ -0,0 +1,32 @@
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from Crypto import Random
+
+random_seed = Random.new().read
+
+keyPair = RSA.generate(1024, random_seed)
+pubKey = keyPair.publickey()
+
+True_text = "Hello Bob"
+Fake_text = "Bye Bob"
+
+hashA = SHA256.new(True_text.encode("utf-8")).digest()
+digitalSign = keyPair.sign(hashA, '')
+
+print("Hash A:" + repr(hashA) + "\n")
+print("Digital signature:" + repr(digitalSign)+"\n")
+
+hashB = SHA256.new(True_text.encode("utf-8")).digest()
+hashC = SHA256.new(Fake_text.encode("utf-8")).digest()
+
+print("HashB :" + repr(hashB) + "\n")
+print("HashC :" + repr(hashC) + "\n")
+
+if(pubKey.verify(hashB, digitalSign)):
+    print("O texto autentico é" + True_text)
+
+elif(pubKey.verify(hashB, digitalSign)):
+    print("O texto autentico é" + Fake_text)
+
+else:
+    print("Nenhum dos textos é autentico")  
